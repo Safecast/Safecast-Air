c17e3d796f432cfbb42290f062fe30b8bffb5bd2
Will Dickson <will@iorodeo.com>
2015-05-12 13:49:38 -0700

    Worked on class for amphenol pm sensor.

---
+++ safecast_air/amphenol_pm_sensor.cpp
@@ -0,0 +1,202 @@
+#include "limits.h"
+#include "amphenol_pm_sensor.h"
+#include "constants.h"
+#include "Arduino.h"
+#include "Streaming.h"
+
+namespace amphenol
+{
+
+    const PMSensorParam UndefinedSensorParam = {32, 33, 1.5, 10000000};
+
+    // Occupancy public methods
+    // --------------------------------------------------------------------------------------------
+
+    OccupancySensor::OccupancySensor()
+    {
+        reset();
+    }
+
+    void OccupancySensor::initialize(int pin, unsigned long sampleWindow, void (*pinChangeHandler)(void))
+    {
+        pin_ = pin;
+        sampleWindow_ = sampleWindow;
+
+        pinMode(pin_, INPUT);
+        attachInterrupt(pin_,pinChangeHandler,CHANGE);
+        reset();
+    }
+
+   void OccupancySensor::reset()
+   {
+       lastChangeTime_ = micros();
+       resetAccum();
+       totalTime_ = 0;
+       highTime_ = 0;
+       lowTime_ = 0;
+       count_ = 0;
+       pinState_ = digitalRead(pin_);
+   }
+
+
+   void OccupancySensor::update()
+   {
+       int newPinState = digitalRead(pin_);
+       unsigned long currentTime = micros();
+
+       if (newPinState == pinState_)
+       {
+           // Pin state hasn't changed.
+           return;
+       }
+
+       
+       // Get time since last pin change
+       unsigned long dt = 0;
+       if (currentTime < lastChangeTime_)
+       {
+           dt = currentTime + (ULONG_MAX - lastChangeTime_);
+       }
+       else
+       {
+           dt = currentTime - lastChangeTime_;
+       }
+
+       // Update  time and count accumulators
+       totalTimeAccum_ += dt;
+       if (newPinState == HIGH)
+       {
+           highTimeAccum_  += dt;
+           countAccum_ += 1;
+       }
+       else
+       {
+           lowTimeAccum_ += dt;
+       }
+
+       // Get values and reset if  sample window has passed
+       if (totalTimeAccum_ >= sampleWindow_)
+       {
+           totalTime_ = totalTimeAccum_;
+           highTime_ = highTimeAccum_;
+           lowTime_ = lowTimeAccum_;
+           count_ = countAccum_;
+           resetAccum();
+       }
+
+       pinState_ = newPinState;
+       lastChangeTime_ = currentTime;
+   }
+
+   float  OccupancySensor::value()
+   {
+       float value = 0.0;
+       unsigned long timeTotal = highTime_ + lowTime_;
+       if (timeTotal != 0)
+       {
+           value = float(lowTime_)/float(timeTotal);
+       }
+       return value;
+   }
+
+   unsigned long OccupancySensor::count()
+   {
+       return count_;
+   }
+
+
+
+    // Occupancy protected methods
+    // --------------------------------------------------------------------------------------------
+
+    void OccupancySensor::resetAccum()
+    {
+       totalTimeAccum_ = 0;
+       highTimeAccum_ = 0;
+       lowTimeAccum_ = 0;
+       countAccum_ = 0;
+    }
+
+
+    // PMSensorDev public methods
+    // --------------------------------------------------------------------------------------------
+    PMSensorDev::PMSensorDev()
+    { 
+        setParam(UndefinedSensorParam);
+    }
+
+    PMSensorDev::PMSensorDev(PMSensorParam param) 
+    {
+        setParam(param);
+    }
+
+    void PMSensorDev::setParam(PMSensorParam param)
+    {
+        param_ = param;
+    }
+
+    PMSensorParam PMSensorDev::param()
+    {
+        return param_;
+    }
+
+    float PMSensorDev::smallParticleOccupancy()
+    {
+        return smallParticleSensor_.value();
+    }
+
+    float PMSensorDev::largeParticleOccupancy()
+    {
+        return largeParticleSensor_.value();
+    }
+
+    unsigned long PMSensorDev::smallParticleCount()
+    {
+        return smallParticleSensor_.count();
+    }
+
+    unsigned long PMSensorDev::largeParticleCount()
+    {
+        return largeParticleSensor_.count();
+    }
+
+    void PMSensorDev::initialize()
+    {
+        smallParticleSensor_.initialize(
+                param_.smallParticlePin, 
+                param_.sampleWindow,
+                PMSensorDev::onSmallParticlePinChange
+                );
+
+        largeParticleSensor_.initialize(
+                param_.largeParticlePin, 
+                param_.sampleWindow,
+                PMSensorDev::onLargeParticlePinChange
+                );
+    }
+
+    void PMSensorDev::update()
+    {
+        smallParticleSensor_.update();
+        largeParticleSensor_.update();
+    }
+
+    // PMSensorDev Protected methods
+    // --------------------------------------------------------------------------------------------
+
+    void PMSensorDev::onSmallParticlePinChange()
+    {
+        PMSensor.update();
+    }
+
+    void PMSensorDev::onLargeParticlePinChange()
+    {
+        PMSensor.update();
+    }
+
+
+} // namespace amphenol
+
+amphenol::PMSensorDev PMSensor(constants::DefaultPMSensorParam);
+
+
---
+++ safecast_air/amphenol_pm_sensor.h
@@ -0,0 +1,90 @@
+#ifndef AMPHENOL_PM_SENSOR_H
+#define AMPHENOL_PM_SENSOR_H
+
+namespace amphenol
+{
+
+    struct PMSensorParam
+    {
+        int smallParticlePin;
+        int largeParticlePin;
+        float sensitivity;
+        unsigned long sampleWindow;
+    };
+
+    class OccupancySensor 
+    {
+        public:
+
+            OccupancySensor();
+            void initialize(int pin, unsigned long sampleWindow, void (*pinChangeFunc)(void)); 
+            void reset();
+            void update();
+            float value();
+            unsigned long count();
+
+        protected:
+
+            int pin_;
+            int pinState_;
+            unsigned long sampleWindow_;
+
+            unsigned long lastChangeTime_;
+            unsigned long totalTimeAccum_;
+            unsigned long highTimeAccum_;
+            unsigned long lowTimeAccum_;
+            unsigned long countAccum_;
+
+            unsigned long totalTime_;
+            unsigned long highTime_;
+            unsigned long lowTime_;
+            unsigned long count_;
+
+            void resetAccum();
+
+    };
+
+
+
+    class PMSensorDev
+    {
+        // Amphenol Smart Dust Sensor SM-PWM-01A 
+
+        public:
+
+            PMSensorDev();
+            PMSensorDev(PMSensorParam param);
+
+            void setParam(PMSensorParam param);
+            PMSensorParam param();
+
+            void initialize();
+            void update();
+
+            float smallParticleOccupancy();
+            float largeParticleOccupancy();
+
+            unsigned long smallParticleCount();
+            unsigned long largeParticleCount();
+
+        protected:
+
+            PMSensorParam param_;
+
+            OccupancySensor smallParticleSensor_;
+            OccupancySensor largeParticleSensor_;
+
+            static void onSmallParticlePinChange();
+            static void onLargeParticlePinChange();
+
+
+    };
+
+} // namespace amphenol
+
+
+extern amphenol::PMSensorDev PMSensor;
+
+#endif
+
+
